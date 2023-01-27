2d45ffbff2f9cc05f4f80cc38b2b22f116eb7290
Dmitrii Smirnov <dismirow@gmail.com>
2023-01-28 00:59:18 +0300

    add new function gendiff

--- gendiff.js
+++ gendiff.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { getDataFromFile } from "./src/gendiff.js";
-import process from 'node:process';
+import { gendiff } from "./src/gendiff.js";
 const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
     .helpOption('-h, --help', 'output usage information')
-    // .option('-h, --help', 'output usage information')
     .option('-f, --format <type>', 'output format')
     .argument('<filepath1>')
     .argument('<filepath2>')
     .action((filepath1, filepath2) => {
-        console.log(getDataFromFile([process.cwd(), filepath1]))
-        console.log(getDataFromFile([process.cwd(), filepath2]))
+        console.log(gendiff(filepath1, filepath2));
     })
     .parse();
 
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,9 +1,31 @@
 import * as fs from 'node:fs'
 import path from 'node:path';
 
-function getDataFromFile(pathToFile) {
-    const rawdata = fs.readFileSync(path.resolve(...pathToFile));
-    return JSON.parse(rawdata);
+function getDataFromFile(filepath) {
+    const correctPath = path.resolve(process.cwd(), filepath)
+    const rawdata = fs.readFileSync(correctPath);
+    switch (path.extname(correctPath)) {
+        case ('.json') :
+            return JSON.parse(rawdata);
+        default : 
+            return 'Данный тип файла не поддерживается!'
+    } 
 }
-
-export {getDataFromFile }
+function gendiff(filepath1, filepath2) {
+    const firstFile = getDataFromFile(filepath1);
+    const secondFile = getDataFromFile(filepath2);
+    const sortedkeys = Object.keys({...firstFile, ... secondFile}).sort();
+    const outputData = sortedkeys.reduce((acc, key) => {
+        if (!Object.hasOwn(firstFile, key)) acc[`+ ${key}`] = secondFile[key];
+        else if (!Object.hasOwn(secondFile, key)) acc[`- ${key}`] = firstFile[key];
+        else if (firstFile[key] === secondFile[key]) acc[`  ${key}`] = firstFile[key];
+        else {
+            acc[`- ${key}`] = firstFile[key];
+            acc[`+ ${key}`] = secondFile[key];
+        }
+        return {...acc};
+    },{})
+    return JSON.stringify(outputData, null, 2).replace(/[,"]/g,'')
+ 
+}
+export { gendiff };
