bacb33ef2e1a982d31ecf1457cfcd4e7c3d25334
Dmitrii Smirnov <dismirow@gmail.com>
2023-01-28 12:50:50 +0300

    add eslint and fix all lint errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import gendiff from '../index.js'
+import { Command } from 'commander';
+import gendiff from '../index.js';
+
 const program = new Command();
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .action((filepath1, filepath2) => {
-        console.log(gendiff(filepath1, filepath2));
-    })
-    .parse();
-
-    
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  })
+  .parse();
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,31 +1,30 @@
-import * as fs from 'node:fs'
+import * as fs from 'node:fs';
 import path from 'node:path';
 
 function getDataFromFile(filepath) {
-    const correctPath = path.resolve(process.cwd(), filepath)
-    const rawdata = fs.readFileSync(correctPath);
-    switch (path.extname(correctPath)) {
-        case ('.json') :
-            return JSON.parse(rawdata);
-        default : 
-            return 'Данный тип файла не поддерживается!'
-    } 
+  const correctPath = path.resolve(process.cwd(), filepath);
+  const rawdata = fs.readFileSync(correctPath);
+  switch (path.extname(correctPath)) {
+    case ('.json'):
+      return JSON.parse(rawdata);
+    default:
+      return 'Данный тип файла не поддерживается!';
+  }
 }
 function gendiff(filepath1, filepath2) {
-    const firstFile = getDataFromFile(filepath1);
-    const secondFile = getDataFromFile(filepath2);
-    const sortedkeys = Object.keys({...firstFile, ... secondFile}).sort();
-    const outputData = sortedkeys.reduce((acc, key) => {
-        if (!Object.hasOwn(firstFile, key)) acc[`+ ${key}`] = secondFile[key];
-        else if (!Object.hasOwn(secondFile, key)) acc[`- ${key}`] = firstFile[key];
-        else if (firstFile[key] === secondFile[key]) acc[`  ${key}`] = firstFile[key];
-        else {
-            acc[`- ${key}`] = firstFile[key];
-            acc[`+ ${key}`] = secondFile[key];
-        }
-        return {...acc};
-    },{})
-    return JSON.stringify(outputData, null, 2).replace(/[,"]/g,'')
- 
+  const firstFile = getDataFromFile(filepath1);
+  const secondFile = getDataFromFile(filepath2);
+  const sortedkeys = Object.keys({ ...firstFile, ...secondFile }).sort();
+  const outputData = sortedkeys.reduce((acc, key) => {
+    if (!Object.hasOwn(firstFile, key)) acc[`+ ${key}`] = secondFile[key];
+    else if (!Object.hasOwn(secondFile, key)) acc[`- ${key}`] = firstFile[key];
+    else if (firstFile[key] === secondFile[key]) acc[`  ${key}`] = firstFile[key];
+    else {
+      acc[`- ${key}`] = firstFile[key];
+      acc[`+ ${key}`] = secondFile[key];
+    }
+    return { ...acc };
+  }, {});
+  return JSON.stringify(outputData, null, 2).replace(/[,"]/g, '');
 }
-export { gendiff };
+export default gendiff;
